284ccb02ae05a5e0c014d18c691e71a5b32d537c
jerry1ye10 <jerry1ye10@gmail.com>
2019-01-31 23:33:10 -0500

    updated makepic.py

--- makepic.py
+++ makepic.py
@@ -1,21 +1,28 @@
-cols = 500 #number of columns(up to down) and measured left to right
-rows = 500#number of rows(left to right) and measured up to down
+cols = 750 #number of columns(up to down) and measured left to right
+rows = 750#number of rows(left to right) and measured up to down
 max = 255
 
 imageString = ''#declaration of image String
 def header(f):#outputs the header
-    f.write('p3\n')#first line of image file
+    f.write('P3\n')#first line of image file JERRRRRRRRRRYYYYY  THIS IS P3 NOT p3
     f.write(str(cols) + ' ' + str(rows) + '\n') #second line of image file(following <column#> <row#>)
     f.write(str(max) + '\n')
 
 def genImage(f):
+    #color = 'red'
     for c in range(cols):
         #print(c)
         for r in range(rows):
-            r = 0
+            red = 0
             g = 0
             b = 0
-            f.write(str(r) + ' ' + str(g) + ' ' + str(b) + '\t')
+            if c <= 250:
+                red = 255
+            elif c <= 500:
+                g = 255
+            else:
+                b = 255
+            f.write(str(red) + ' ' + str(g) + ' ' + str(b) + ' ') # WHY ARE U USING \t instead of ' '
         f.write('\n')
 f = open("pic.ppm","w")#opens image file that will be written in
 header(f)
